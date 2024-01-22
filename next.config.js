ac4d0be0e71d52eb3560cde94f45109602d835d6
NoelKasemi <noelkasemi0@gmail.comm>
2024-01-22 14:21:01 +0100

    Update `next.config.js` to set the `publicPath` for static export.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  output: {
+      // Set the path for static export
+      publicPath: '/',
+    },
+  };
+  
