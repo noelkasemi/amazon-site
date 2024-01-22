38b9a898f0978358a6f3ae0c55c9ccfbc11d904f
NoelKasemi <noelkasemi0@gmail.comm>
2024-01-22 14:26:14 +0100

    Changed the output setting from "exports" to "export" in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
  /** @type {import('next').NextConfig} */
  const nextConfig = {
-    output: "exports", // lightweight but no dynamic routes & api support
+    output: "export", // lightweight but no dynamic routes & api support
     // output: "standalone", // bulkier but supports dynamic routes & api
 }
 
