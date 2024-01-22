85d2f1416b4aa9ca84a96cd1f9effe1ea2c2b23f
NoelKasemi <noelkasemi0@gmail.comm>
2024-01-22 14:24:35 +0100

    Update Next.js config to use "exports" output mode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  output: {
-      // Set the path for static export
-      publicPath: '/',
-    },
-  };
-  
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+    output: "exports", // lightweight but no dynamic routes & api support
+    // output: "standalone", // bulkier but supports dynamic routes & api
+}
+
+module.exports = nextConfig
