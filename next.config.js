8c814e7d5794b80932df91ca4fe3566a413cb336
NoelKasemi <noelkasemi0@gmail.comm>
2024-01-22 19:42:03 +0100

    Update app layout and next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
- /** @type {import('next').NextConfig} */
- const nextConfig = {
-    output: "exports", // lightweight but no dynamic routes & api support
-    // output: "standalone", // bulkier but supports dynamic routes & api
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
+ 
 module.exports = nextConfig
